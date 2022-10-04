4991c4638e88854b4986b85636329bbb5e941bba
MatheusMathos <matheusmatos88x10@gmail.com>
2022-10-04 14:49:44 -0300

    Commit 1, ja criado os components - AddTask, AddTask, Button, etc...

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import "./App.css";
+
+
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Ler livro',
+      completed: true,
+    },
+  ]);
+
+  return (
+    <>
+      <div className="container">
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
+
+    </>
+
+  );
+};
+
+export default App;
