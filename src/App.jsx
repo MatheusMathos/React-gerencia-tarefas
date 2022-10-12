c780f760753813cc6f9b5f917532ee01b5adb78b
MatheusMathos <matheusmatos88x10@gmail.com>
2022-10-12 17:13:15 -0300

    Adicionado o componente TaskDetails.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import TaskDetails from "./components/TaskDetails"
+
 import "./App.css";
 
 
@@ -48,15 +51,27 @@ const App = () => {
   };
 
   return (
-    <>
+    <Router>
       <div className="container">
         <Header />
-        <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks tasks={tasks} handleTaskClick={handleTaskClick}
-        handleTaskDeletion={handleTaskDelition} />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <AddTask handleTaskAddition={handleTaskAddition} />
+              <Tasks
+                tasks={tasks}
+                handleTaskClick={handleTaskClick}
+                handleTaskDeletion={handleTaskDelition}
+              />
+            </>
+          )}
+        />
+        <Route path="/2121" exact render={TaskDetails}/>
       </div>
 
-    </>
+    </Router>
 
   );
 };
---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Button from './Button';
+
+const TaskDetails = () => {
+    return (
+        <>
+        <div className="back-buttom-container">
+            <Button>Voltar</Button>
+        </div>
+        <div className="task-details-container">
+            <p></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere provident ullam sit tenetur, ad tempora?</p>
+        </div>
+        </>
+      );
+}
+ 
+export default TaskDetails;
