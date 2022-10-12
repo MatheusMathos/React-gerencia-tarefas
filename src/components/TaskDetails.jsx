47135dedc5b45209ab3d347edb2a45e8a9b5d8c1
MatheusMathos <matheusmatos88x10@gmail.com>
2022-10-12 17:48:42 -0300

    Estilizando o TaksDetails.

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import Button from './Button';
 
 const TaskDetails = () => {
+    const params = useParams();
+
+    console.log(params);
+
     return (
         <>
         <div className="back-buttom-container">
             <Button>Voltar</Button>
         </div>
         <div className="task-details-container">
-            <p></p>
+            <h2>{params.taskTitle}</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere provident ullam sit tenetur, ad tempora?</p>
         </div>
         </>
